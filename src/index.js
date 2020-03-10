c6dedf27cf429e80b5463770d34c1e3f649d2a7a
Jp1200 <jprkr96@gmail.com>
2020-03-10 12:07:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const input = document.getElementById('create-task-form');
+
+  input.addEventListener('submit',submitForm);
+
+  function submitForm(){
+    const val = document.getElementById("new-task-description").value;
+    const li = document.createElement('li');
+    li.innerHTML =  `${val} ` +  '<button id="Delete">Finish Task</button>';
+    document.getElementById("create-task-form").reset();
+    document.getElementById('tasks').appendChild(li);
+
+    event.preventDefault();
+
+  }
+  if document.getElementById('Delete'){
+    const del = document.getElementById('Delete')
+    del.addEventListener('click',()=>{
+      del.parentNode.parentNode.removeChild();
+  });
+  }
 });
