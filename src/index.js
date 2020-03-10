64412063dee3f458106a57286cc95ce1c1b5c57d
github username <github email address>
2020-03-10 12:07:20 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById('create-task-form');
+  const enteredText = document.getElementById('new-task-description');
+  const tasks = document.getElementById('tasks');
+  // let deletButton = document.getElementsByClassName('delete');
+
+  taskForm.addEventListener('submit', function(event){
+    event.preventDefault();
+  
+    let li = document.createElement('li');
+    li.innerText = enteredText.value 
+    tasks.appendChild(li);
+
+    // let deleteButton = document.createElement('button');
+    // deleteButton.className = 'delete';
+    // deleteButton.innerText = 'x';
+
+    // li.appendChild(deleteButton);
+  })
+
+  // deleteButton.addEventListener('click', function(){
+    
+  // })
 });
+
+
+
+
+
+
+
+
