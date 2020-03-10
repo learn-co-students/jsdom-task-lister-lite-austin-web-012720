ee1cc1271897c6debbc630156c5c583538309a77
qudroturkey <reesemb1@gmail.com>
2020-03-10 11:17:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const appendNewTask = task => {
+  document.getElementById('tasks').appendChild(task);
+};
+
+const createNewTask = (event) => {
+  event.preventDefault();
+
+  const newTaskDescription = document.getElementById('new-task-description');
+  const newTask = document.createElement('li');
+
+  newTask.innerText = newTaskDescription.value;
+  appendNewTask(newTask);
+
+  event.target.reset();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const newTaskForm = document.getElementById('create-task-form');
+  newTaskForm.addEventListener('submit', createNewTask);
 });
