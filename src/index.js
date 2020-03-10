07b8d3faf99cffb2d63138e548e34d642d1ade98
github username <github email address>
2020-03-10 11:10:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const toDoList = [];
+  const formText = document.getElementById('create-task-form')
+  const submitButton = document.querySelectorAll('input')[1]
+
+  formText.addEventListener('submit', function(event) {
+    event.preventDefault();
+    addToDo(document.getElementById('new-task-description').value);
+    displayToDo();
+  });
+
+  function addToDo(toDo){
+    toDoList.push(toDo);
+  }
+
+  function displayToDo(){
+    list = document.getElementById('tasks');
+    list.innerHTML = '';
+    for (let i = 0; i < toDoList.length; i++){
+      let toDoItem = document.createElement('li');
+      toDoItem.innerText = toDoList[toDoList.length - 1 - i];
+      list.appendChild(toDoItem);
+    }
+
+  }
 });
