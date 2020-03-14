f977cc07953deecffb2242e9f8b3a1c65d0944b9
github username <github email address>
2020-03-14 14:41:27 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   const taskForm = document.getElementById('create-task-form');
-  const enteredText = document.getElementById('new-task-description');
-  const tasks = document.getElementById('tasks');
-  // let deletButton = document.getElementsByClassName('delete');
 
-  taskForm.addEventListener('submit', function(event){
+  let enteredText = document.getElementById('new-task-description');
+  
+  const tasks = document.getElementById('tasks');
+ 
+
+  taskForm.addEventListener('submit', (event) => {
     event.preventDefault();
   
     let li = document.createElement('li');
+    //line 14 is exactly the same as line 15, one if just using the event.target and the other caputure the value directly
+    //li.innerText = event.target.children[1].value;
     li.innerText = enteredText.value 
     tasks.appendChild(li);
+    enteredText.value = "";
 
-    // let deleteButton = document.createElement('button');
-    // deleteButton.className = 'delete';
-    // deleteButton.innerText = 'x';
+    let deleteButton = document.createElement('button');
+    deleteButton.setAttribute('class','delete');
+    deleteButton.innerText = ' x ';
 
-    // li.appendChild(deleteButton);
-  })
+    li.appendChild(deleteButton);
 
-  // deleteButton.addEventListener('click', function(){
-    
-  // })
+    deleteButton.addEventListener("click", (event) => {
+      let ul = li.parentNode;
+      ul.removeChild(li);
+    });
+
+  });
+
 });
+
 
 
 
